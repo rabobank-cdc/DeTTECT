5c700690c6caa1bbf7a01c75fcc11b7408933298
Marcus Bakker <marcus@mbsecure.nl>
2019-08-13 14:30:43 +0200

    Added the possibility to use EQL queries.

--- interactive_menu.py
+++ interactive_menu.py
@@ -2,8 +2,7 @@ import glob
 from data_source_mapping import *
 from technique_mapping import *
 from group_mapping import *
-from constants import *
-
+from eql_yaml import *
 
 groups = 'all'
 software_group = False
@@ -12,8 +11,11 @@ default_stage = 'attack'
 default_matrix = 'enterprise'
 groups_overlay = ''
 overlay_type = 'group'
-filter_applicable_to = 'all'
 yaml_path = 'sample-data/'
+eql_all_scores = False
+eql_query_detection = None
+eql_query_visibility = None
+eql_query_data_sources = None
 
 
 def _clear():
@@ -201,6 +203,8 @@ def _menu_statistics():
     print('')
     print('Options:')
     print('1. Matrix: %s' % default_matrix)
+    print('')
+    print('Select what you want to do:')
     print('2. Get a sorted count on how many ATT&CK Enterprise techniques are covered by a particular Data Source.')
     print('3. Get a sorted count on how many ATT&CK Enterprise or Mobile techniques are covered by a Mitigation.')
     print('9. Back to main menu.')
@@ -228,39 +232,56 @@ def _menu_data_source(filename_ds):
     :param filename_ds:
     :return:
     """
+    global eql_query_data_sources
     _clear()
     print('Menu: %s' % MENU_NAME_DATA_SOURCE_MAPPING)
     print('')
     print('Selected data source YAML file: %s' % filename_ds)
     print('')
+    print('Options:')
+    eql_ds_str = '' if not eql_query_data_sources else eql_query_data_sources
+    print('1. Only include data sources which match the provided EQL query: ' + eql_ds_str)
+    print('')
     print('Select what you want to do:')
-    print('1. Generate a data source layer for the ATT&CK Navigator.')
-    print('2. Generate a graph with data sources added through time.')
-    print('3. Generate an Excel sheet with all data sources.')
-    print('4. Generate a technique administration YAML file with visibility scores, based on the number of available '
+    print('2. Generate a data source layer for the ATT&CK Navigator.')
+    print('3. Generate a graph with data sources added through time.')
+    print('4. Generate an Excel sheet with all data sources.')
+    print('5. Generate a technique administration YAML file with visibility scores, based on the number of available '
           'data sources')
-    print('5. update the visibility scores within a technique administration YAML file based on changes within any of '
+    print('6. update the visibility scores within a technique administration YAML file based on changes within any of '
           'the data sources. \nPast visibility scores are preserved in the score_logbook, and manually assigned scores are '
           'not updated without your approval. \nThe updated visibility are based on the number of available data sources.')
     print('9. Back to main menu.')
     choice = _ask_input()
     if choice == '1':
-        print('Writing data sources layer...')
-        generate_data_sources_layer(filename_ds)
-        _wait()
-    elif choice == '2':
-        print('Drawing the graph...')
-        plot_data_sources_graph(filename_ds)
-        _wait()
-    elif choice == '3':
-        print('Generating Excel file...')
-        export_data_source_list_to_excel(filename_ds)
-        _wait()
-    elif choice == '4':
-        print('Generating YAML file...')
-        generate_technique_administration_file(filename_ds)
-        _wait()
-    elif choice == '5':
+        print('Specify the EQL query for data source objects:')
+        eql_query_data_sources = _ask_input().lower()
+
+    elif choice in ['2', '3', '4', '5']:
+        file_ds = filename_ds
+
+        if eql_query_data_sources:
+            file_ds = search(filename_ds, FILE_TYPE_DATA_SOURCE_ADMINISTRATION, eql_query_data_sources)
+            if not file_ds:
+                _wait()  # something went wrong in executing the search or 0 results where returned
+                _menu_data_source(filename_ds)
+        if choice == '2':
+            print('Writing data sources layer...')
+            generate_data_sources_layer(file_ds)
+            _wait()
+        elif choice == '3':
+            print('Drawing the graph...')
+            plot_data_sources_graph(file_ds)
+            _wait()
+        elif choice == '4':
+            print('Generating Excel file...')
+            export_data_source_list_to_excel(file_ds)
+            _wait()
+        elif choice == '5':
+            print('Generating YAML file...')
+            generate_technique_administration_file(file_ds)
+            _wait()
+    elif choice == '6':
         filename_t = _select_file(MENU_NAME_DETECTION_COVERAGE_MAPPING, 'techniques (used to score the level of visibility)',
                                   FILE_TYPE_TECHNIQUE_ADMINISTRATION, False)
         print('Updating visibility scores...')
@@ -279,47 +300,68 @@ def _menu_detection(filename_t):
     :param filename_t:
     :return:
     """
-    global filter_applicable_to
+    global eql_all_scores, eql_query_detection, eql_query_visibility
+
+    filename_str = filename_t
     _clear()
     print('Menu: %s' % MENU_NAME_DETECTION_COVERAGE_MAPPING)
     print('')
-    print('Selected techniques YAML file: %s' % filename_t)
+    print('Selected techniques YAML file: %s' % filename_str)
     print('')
     print('Options:')
-    print('1. Filter techniques based on the \'applicable_to\' field in the technique administration YAML file (not '
-          'for Excel output): %s' % filter_applicable_to)
+    eql_d_str = '' if not eql_query_detection else eql_query_detection
+    eql_v_str = '' if not eql_query_visibility else eql_query_visibility
+    print('1. Only include detection objects which match the EQL query: ' + eql_d_str)
+    print('2. Only include visibility objects which match the EQL query: ' + eql_v_str)
+    print('3. Include all \'score\' objects from the \'score_logbook\' in the EQL search: ' + str(eql_all_scores))
     print('')
     print('Select what you want to do:')
-    print('2. Generate a layer for detection coverage for the ATT&CK Navigator.')
-    print('3. Generate a layer for detection coverage overlaid with visibility for the ATT&CK Navigator.')
-    print('4. Generate a graph with detections added through time.')
-    print('5. Generate an Excel sheet with all administrated techniques.')
-    print('6. Check the technique YAML file for errors.')
+    print('4. Generate a layer for detection coverage for the ATT&CK Navigator.')
+    print('5. Generate a layer for detection coverage overlaid with visibility for the ATT&CK Navigator.')
+    print('6. Generate a graph with detections added through time.')
+    print('7. Generate an Excel sheet with all administrated techniques.')
+    print('8. Check the technique YAML file for errors.')
     print('9. Back to main menu.')
     choice = _ask_input()
     if choice == '1':
-        print('Specify your filter for the applicable_to field:')
-        filter_applicable_to = _ask_input().lower()
+        print('Specify the EQL query for detection objects:')
+        eql_query_detection = _ask_input().lower()
     elif choice == '2':
-        print('Writing detection coverage layer...')
-        generate_detection_layer(filename_t, None, False, filter_applicable_to)
-        _wait()
+        print('Specify the EQL query for visibility objects:')
+        eql_query_visibility = _ask_input().lower()
     elif choice == '3':
-        filename_ds = _select_file(MENU_NAME_DETECTION_COVERAGE_MAPPING, 'data sources (used to add metadata on the '
-                                                                         'involved data sources to the heat map)',
-                                   FILE_TYPE_DATA_SOURCE_ADMINISTRATION, False)
-        print('Writing detection coverage layer with visibility as overlay...')
-        generate_detection_layer(filename_t, filename_ds, True, filter_applicable_to)
-        _wait()
-    elif choice == '4':
-        print('Drawing the graph...')
-        plot_detection_graph(filename_t, filter_applicable_to)
-        _wait()
-    elif choice == '5':
-        print('Generating Excel file...')
-        export_techniques_list_to_excel(filename_t)
-        _wait()
-    elif choice == '6':
+        print('Specify True or False for to include all scores or not:')
+        eql_all_scores = True if _ask_input().lower() == 'true' else False
+
+    elif choice in ['4', '5', '6', '7']:
+        file_tech = filename_t
+
+        if eql_query_detection or eql_query_visibility:
+            file_tech = techniques_search(filename_t, eql_query_visibility, eql_query_detection,
+                                          include_all_score_objs=eql_all_scores)
+            if not file_tech:
+                _wait()  # something went wrong in executing the search or 0 results where returned
+                _menu_detection(filename_t)
+        if choice == '4':
+            print('Writing detection coverage layer...')
+            generate_detection_layer(file_tech, None, False)
+            _wait()
+        elif choice == '5':
+            filename_ds = _select_file(MENU_NAME_DETECTION_COVERAGE_MAPPING, 'data sources (used to add metadata on the '
+                                                                             'involved data sources to the heat map)',
+                                       FILE_TYPE_DATA_SOURCE_ADMINISTRATION, False)
+            print('Writing detection coverage layer with visibility as overlay...')
+            generate_detection_layer(file_tech, filename_ds, True)
+            _wait()
+        elif choice == '6':
+            print('Drawing the graph...')
+            plot_detection_graph(file_tech)
+            _wait()
+        elif choice == '7':
+            print('Generating Excel file...')
+            export_techniques_list_to_excel(file_tech)
+            _wait()
+    elif choice == '8`x':
         print('Checking the technique YAML file for errors...')
         check_yaml_file_health(filename_t, FILE_TYPE_TECHNIQUE_ADMINISTRATION, health_is_called=True)
         _wait()
@@ -337,43 +379,64 @@ def _menu_visibility(filename_t, filename_ds):
     :param filename_ds:
     :return:
     """
-    global filter_applicable_to
+    global eql_all_scores, eql_query_detection, eql_query_visibility
+
+    filename_str = filename_t
     _clear()
     print('Menu: %s' % MENU_NAME_VISIBILITY_MAPPING)
     print('')
-    print('Selected techniques YAML file: %s' % filename_t)
+    print('Selected techniques YAML file: %s' % filename_str)
     print('Selected data source YAML file: %s' % filename_ds)
     print('')
     print('Options:')
-    print('1. Filter techniques based on the \'applicable_to\' field in the technique administration YAML file (not for '
-          'Excel output): %s' % filter_applicable_to)
+    eql_d_str = '' if not eql_query_detection else eql_query_detection
+    eql_v_str = '' if not eql_query_visibility else eql_query_visibility
+    print('1. Only include visibility objects which match the EQL query: ' + eql_v_str)
+    print('2. Only include detection objects which match the EQL query: ' + eql_d_str)
+    print('3. Include all \'score\' objects from the \'score_logbook\' in the EQL search: ' + str(eql_all_scores))
     print('')
     print('Select what you want to do:')
-    print('2. Generate a layer for visibility for the ATT&CK Navigator.')
-    print('3. Generate a layer for visibility overlaid with detection coverage for the ATT&CK Navigator.')
-    print('4. Generate an Excel sheet with all administrated techniques.')
-    print('5. Check the technique YAML file for errors.')
+    print('4. Generate a layer for visibility for the ATT&CK Navigator.')
+    print('5. Generate a layer for visibility overlaid with detection coverage for the ATT&CK Navigator.')
+    print('6. Generate an Excel sheet with all administrated techniques.')
+    print('7. Check the technique YAML file for errors.')
     print('9. Back to main menu.')
     choice = _ask_input()
     if choice == '1':
-        print('Specify your filter for the applicable_to field:')
-        filter_applicable_to = _ask_input().lower()
+        print('Specify the EQL query for visibility objects:')
+        eql_query_visibility = _ask_input().lower()
     elif choice == '2':
-        print('Writing visibility coverage layer...')
-        generate_visibility_layer(filename_t, filename_ds, False, filter_applicable_to)
-        _wait()
+        print('Specify the EQL query for detection objects:')
+        eql_query_detection = _ask_input().lower()
     elif choice == '3':
-        print('Writing visibility coverage layer overlaid with detections...')
-        generate_visibility_layer(filename_t, filename_ds, True, filter_applicable_to)
-        _wait()
-    elif choice == '4':
-        print('Generating Excel file...')
-        export_techniques_list_to_excel(filename_t)
-        _wait()
-    elif choice == '5':
-        print('Checking the technique YAML file for errors...')
-        check_yaml_file_health(filename_t, FILE_TYPE_TECHNIQUE_ADMINISTRATION, health_is_called=True)
-        _wait()
+        print('Specify True or False for to include all scores or not:')
+        eql_all_scores = True if _ask_input().lower() == 'true' else False
+
+    elif choice in ['4', '5', '6', '7']:
+        file_tech = filename_t
+
+        if eql_query_detection or eql_query_visibility:
+            file_tech = techniques_search(filename_t, eql_query_visibility, eql_query_detection,
+                                          include_all_score_objs=eql_all_scores)
+            if not file_tech:
+                _wait()  # something went wrong in executing the search or 0 results where returned
+                _menu_visibility(filename_t, filename_ds)
+        if choice == '4':
+            print('Writing visibility coverage layer...')
+            generate_visibility_layer(file_tech, filename_ds, False)
+            _wait()
+        elif choice == '5':
+            print('Writing visibility coverage layer overlaid with detections...')
+            generate_visibility_layer(file_tech, filename_ds, True)
+            _wait()
+        elif choice == '6':
+            print('Generating Excel file...')
+            export_techniques_list_to_excel(file_tech)
+            _wait()
+        elif choice == '7':
+            print('Checking the technique YAML file for errors...')
+            check_yaml_file_health(file_tech, FILE_TYPE_TECHNIQUE_ADMINISTRATION, health_is_called=True)
+            _wait()
     elif choice == '9':
         interactive_menu()
     elif choice == 'q':
@@ -386,7 +449,8 @@ def _menu_groups():
     Prints and handles the Threat actor group mapping functionality.
     :return:
     """
-    global groups, software_group, default_platform, default_stage, groups_overlay, overlay_type, filter_applicable_to
+    global groups, software_group, default_platform, default_stage, groups_overlay, overlay_type, eql_all_scores, \
+        eql_query_detection, eql_query_visibility
     _clear()
     print('Menu: %s' % MENU_NAME_THREAT_ACTOR_GROUP_MAPPING)
     print('')
@@ -398,9 +462,15 @@ def _menu_groups():
     print('5. Overlay: ')
     print('    - %s: %s' % ('File' if os.path.exists(groups_overlay) else 'Groups', groups_overlay))
     print('    - Type: %s' % overlay_type)
-    print('6. Filter techniques in the detection or visibility overlay based on the \'applicable_to\' field in the '
-          'technique administration YAML file: %s' % filter_applicable_to)
+    print('    - Type: %s' % overlay_type)
+    print('6. EQL search: ')
+    eql_d_str = '' if not eql_query_detection else eql_query_detection
+    eql_v_str = '' if not eql_query_visibility else eql_query_visibility
+    print('    - Only include detection objects which match the EQL query: ' + eql_d_str)
+    print('    - Only include visibility objects which match the EQL query: ' + eql_v_str)
+    print('    - Include all \'score\' objects from the \'score_logbook\' in the EQL search: ' + str(eql_all_scores))
     print('')
+    print('Select what you want to do:')
     print('7. Generate a heat map layer.')
     print('9. Back to main menu.')
     choice = _ask_input()
@@ -443,10 +513,28 @@ def _menu_groups():
             overlay_type = ''
             groups_overlay = ''
     elif choice == '6':
-        print('Specify your filter for the applicable_to field:')
-        filter_applicable_to = _ask_input().lower()
+        print('')
+        print('1. Only include detection objects which match the EQL query: ' + eql_d_str)
+        print('2. Only include visibility objects which match the EQL query: ' + eql_v_str)
+        print('3. Include all \'score\' objects from the \'score_logbook\' in the EQL search: ' + str(eql_all_scores))
+        choice = _ask_input()
+
+        if choice == '1':
+            print('Specify the EQL query for detection objects:')
+            eql_query_detection = _ask_input().lower()
+        elif choice == '2':
+            print('Specify the EQL query for visibility objects:')
+            eql_query_visibility = _ask_input().lower()
+        elif choice == '3':
+            print('Specify True or False for to include all scores or not:')
+            eql_all_scores = True if _ask_input().lower() == 'true' else False
+
     elif choice == '7':
-        generate_group_heat_map(groups, groups_overlay, overlay_type, default_stage, default_platform, software_group, filter_applicable_to)
+        if not generate_group_heat_map(groups, groups_overlay, overlay_type, default_stage, default_platform,
+                                       software_group, eql_query_visibility, eql_query_detection, False,
+                                       include_all_score_objs=eql_all_scores):
+            _wait()
+            _menu_groups()
         _wait()
     elif choice == '9':
         interactive_menu()
