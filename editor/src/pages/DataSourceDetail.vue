bef1da05d63cf73a59115ccd91f550e6e95b28d5
ruben <ruben@siriussecurity.nl>
2021-10-15 10:09:36 +0200

    Improved the autofill dropdown for data sources to only show data sources which apply to the selected ATT&CK platforms.

--- editor/src/pages/DataSourceDetail.vue
+++ editor/src/pages/DataSourceDetail.vue
@@ -5,7 +5,7 @@
             :item="dataSource"
             itemIdName="data_source_name"
             :allItems="allDataSources"
-            :suggestionList="dataSourceSuggestionList"
+            :suggestionList="makeDataSourceSuggestionList()"
             :navigateItem="navigateItem"
         ></auto-suggest-title>
         <applicable-to-collapse-data-sources
@@ -23,17 +23,20 @@
 <script>
 import ApplicableToCollapseDataSources from '@/components/Inputs/ApplicableToCollapseDataSources';
 import AutoSuggestTitle from '@/components/Inputs/AutoSuggestTitle';
-import Icons from '@/components/Icons';
 import dataSources from '@/data/data_sources';
 import customDataSources from '@/data/custom_data_sources';
+import dataSourcePlatforms from '@/data/data_source_platforms';
 import { pageDetailMixin } from '../mixins/PageDetailMixins.js';
 import _ from 'lodash';
 
 export default {
     data() {
         return {
-            dataSourceSuggestionList: dataSources.concat(customDataSources).sort()
+            selectedPlatforms: Array
         };
+    },
+    created: function () {
+        this.getSelectedPlatforms();
     },
     mixins: [pageDetailMixin],
     props: {
@@ -70,10 +73,38 @@ export default {
         closeAllCollapses() {
             this.$refs.collapseDataSourceComponent.closeAllCollapses();
         },
+        getSelectedPlatforms() {
+            let selectedPlatforms = new Set();
+            for (let i = 0; i < this.allSystems.length; i++) {
+                for (let j = 0; j < this.allSystems[i].platform.length; j++) {
+                    selectedPlatforms.add(this.allSystems[i].platform[j]);
+                }
+            }
+            this.selectedPlatforms = Array.from(selectedPlatforms)
+        },
+        makeDataSourceSuggestionList() {
+            // Make the data source suggestionlist based on both data sources and custom data sources and check if the platform of these
+            // (custom) data sources corresponds to the selected platforms within the systems key-value pair.
+            let suggestionList = new Set()
+            for(let i = 0; i < this.selectedPlatforms.length; i++){
+                for(let j = 0; j < dataSources.length; j++) {
+                    if(dataSourcePlatforms['ATT&CK'][this.selectedPlatforms[i]].includes(dataSources[j])) {
+                        suggestionList.add(dataSources[j]);
+                    }
+                }
+
+                for(let j = 0; j < customDataSources.length; j++) {
+                    if(dataSourcePlatforms['DeTT&CT'][this.selectedPlatforms[i]].includes(customDataSources[j])) {
+                        suggestionList.add(customDataSources[j]);
+                    }
+                }
+            }
+
+            return Array.from(suggestionList).sort()
+        }
     },
     components: {
         AutoSuggestTitle,
-        Icons,
         ApplicableToCollapseDataSources
     }
 };
